3ddf9ae9c08a46a94f28e600fac1537a7b008c73
Dheeraj2407 <kumaaar48@gmail.com>
2019-09-30 20:13:09 +0530

    Bug fix

--- delete.php
+++ delete.php
@@ -25,7 +25,6 @@
     curl_close($ch);
       
       /*End*/
-    #$cn->query("DELETE FROM CUSTOMER WHERE EMAIL='$email'");*/
-    echo "<script>alert('Account deleted successfully');";
-    #location.href='home2.php'</script>";
+    $cn->query("DELETE FROM CUSTOMER WHERE EMAIL='$email'");
+    echo "<script>alert('Account deleted successfully');location.href='home2.php'</script>";
 ?> 
