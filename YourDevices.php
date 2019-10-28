786d914c8fd904bb929f7b2bd111d1a0be5ccf70
Dheeraj2407 <kumaaar48@gmail.com>
2019-10-28 11:33:27 +0530

    Added working iframe

--- YourDevices.php
+++ YourDevices.php
@@ -40,8 +40,8 @@
           
         }
         else{
-          echo '<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/'.$c["channelid"].'/charts/'.$i.'?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>';
-          echo $i;
+          echo '<figure><iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/'.$c["channelid"].'/charts/'.$i.'?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"></iframe>';
+          echo "<figcaption><b>$fields[$i]</b></figcaption></figure>";
         }
       }
     }
